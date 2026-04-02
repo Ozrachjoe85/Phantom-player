846a282d7a29c278a6e9b55b5c3d2e14ccb40ca3
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-04-02 11:53:44 -0600

    Update MainActivity.kt

--- app/src/main/java/com/phantom/player/MainActivity.kt
+++ app/src/main/java/com/phantom/player/MainActivity.kt
@@ -3,17 +3,9 @@ package com.phantom.player
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.phantom.player.ui.screens.LibraryScreen
-import com.phantom.player.ui.screens.PlayerScreen
-import com.phantom.player.ui.screens.EqScreen
-import com.phantom.player.ui.theme.PhantomBlack
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import com.phantom.player.ui.PhantomApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,33 +13,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PhantomPlayerApp()
-        }
-    }
-}
-
-@Composable
-fun PhantomPlayerApp() {
-    val navController = rememberNavController()
-    
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = PhantomBlack
-    ) {
-        NavHost(
-            navController = navController,
-            startDestination = "library"
-        ) {
-            composable("library") {
-                LibraryScreen()
-            }
-            
-            composable("player") {
-                PlayerScreen()
-            }
-            
-            composable("eq") {
-                EqScreen()
+            MaterialTheme {
+                Surface {
+                    PhantomApp()
+                }
             }
         }
     }
